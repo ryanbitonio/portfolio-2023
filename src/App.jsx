91ac5c417fa560fa5b0eee745dc4f0afd5c851a6
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-06-19 01:32:45 +0800

    Creating a responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Grid
+      h="100vh"
+      templateAreas={{
+        base: `"nav" "hero" "main" "footer"`,
+        lg: `"nav hero" "aside hero" "aside main" "footer footer"`,
+      }}
+      templateRows={{
+        base: "58px 300px 1fr 54px",
+        lg: "163px 193px 1fr 141px",
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "460px 1fr",
+      }}
+    >
+      <GridItem area="nav" bg="purple">
+        Nav
+      </GridItem>
+      <GridItem area="hero" bg="gold">
+        Hero
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="dodgerblue">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="coral">
+        Main
+      </GridItem>
+      <GridItem area="footer" bg="green">
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
