d1ea7c98ed44a18205741f21ff28825a95fd0e17
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-07-03 23:46:19 +0800

    Refactoring the grid columns of project grid

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -5,19 +5,16 @@ import ProjectCard from "./ProjectCard";
 const ProjectGrid = () => {
   const { data: projects } = useProjects();
   return (
-    <Flex
-      direction="column"
-      align={{ base: "space-around", md: "flex-start" }}
-      justify={"center"}
-    >
+    <Flex direction="column" align={{ base: "space-around", md: "default" }}>
       <HStack alignSelf="flex-start" pb={5}>
         <Text fontWeight="semibold" fontSize="5xl" letterSpacing="tight">
           Featured Projects Today
         </Text>
       </HStack>
       <SimpleGrid
-        columns={{ base: "1", sm: "2", md: "3", lg: "4", xl: "5" }}
-        spacing={{ base: "11", md: "22" }}
+        justify={"center"}
+        gridTemplateColumns="repeat(auto-fit, minmax(247px, 1fr))"
+        spacing={3}
       >
         {projects?.map(project => (
           <ProjectCard
