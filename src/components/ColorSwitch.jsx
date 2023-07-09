5dba273b9b3a4f74d2407bb0a73b4f2a1d1542cf
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-07-10 00:18:04 +0800

    Adding hover animation to color switch

--- src/components/ColorSwitch.jsx
+++ src/components/ColorSwitch.jsx
@@ -3,13 +3,17 @@ import { IconButton, useColorMode } from "@chakra-ui/react";
 
 const ColorSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const hoverColor = colorMode === "dark" ? "#ffffff1c" : "#00000016";
 
   return (
     <IconButton
+      _hover={{
+        bg: hoverColor,
+      }}
       onClick={toggleColorMode}
       fontSize="5xl"
       variant="outline"
-      icon={<SunIcon />}
+      icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
     >
       ColorSwitch
     </IconButton>
