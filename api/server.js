bdc4edf7acfc7f357a95e5df5162a2b433fc5acd
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-07-19 04:25:19 +0800

    Changing the vercel config

--- api/server.js
+++ api/server.js
@@ -1,16 +1,18 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data/db.json");
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
 
-module.exports = server
+module.exports = server;
