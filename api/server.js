106ff9b6b0bb6bf9dd9496706aed8723b7fbc908
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-07-19 12:53:12 +0800

    Updating vercel config

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 import jsonServer from "json-server";
 const server = jsonServer.create();
-const router = jsonServer.router("./data/db.json");
+const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
 
